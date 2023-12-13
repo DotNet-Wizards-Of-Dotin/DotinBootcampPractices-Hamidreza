d6f03f16189db1d4b8da8ad62d42f83807db2455
Hamidreza Pakpour <hamidrezapakpour95@gmail.com>
2023-12-13 09:58:06 +0330

    GetAllSubFolders class created GetAllFiles method created

--- Get a SubFolders From a Folder in System ( Recursive Methods )/Program.cs
+++ Get a SubFolders From a Folder in System ( Recursive Methods )/Program.cs
@@ -7,4 +7,22 @@
             Console.WriteLine("Hello, World!");
         }
     }
+    public static class GetAllSubFolders
+    {
+        public static List<string> GetAllFiles(string path)
+        {
+            List<string> resault = new List<string>();
+            foreach (var file in Directory.GetFiles(path))
+            {
+                resault.Add(file);
+            }
+
+            foreach (var directory in Directory.GetDirectories(path))
+            {
+                resault.AddRange(GetAllFiles(directory));
+
+            }
+            return resault;
+        }
+    }
 }
