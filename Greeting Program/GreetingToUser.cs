6d23f107af6c85fc16f397116e8559462e1867f6
Hamidreza Pakpour <hamidrezapakpour95@gmail.com>
2023-12-13 11:28:34 +0330

    greeting class seperated

---
+++ Greeting Program/GreetingToUser.cs
@@ -0,0 +1,54 @@
+﻿namespace Greeting_Program
+{
+    public static class GreetingToUser
+    {
+
+        /// <summary>
+        /// به هم چسباندن و بزرگ کردن حرف اول اسم 
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <returns></returns>
+
+        public static string Abbreviation(string firstName, string lastName)
+        {
+            string firstNameAbbreviation = firstName.Substring(0, 1).ToUpper();
+            string lastNameAbbreviation = lastName.Substring(0, 1).ToUpper();
+
+            string abbreviation = $"Welcome {firstNameAbbreviation}.{lastNameAbbreviation}";
+
+            return abbreviation;
+        }
+
+
+        /// <summary>
+        /// خوش امد گویی بر طبق زمان روز
+        /// </summary>
+        /// <param name="firstName"></param>
+        /// <param name="lastName"></param>
+        /// <returns></returns>
+        public static void Greeting(string firstname, string lastname)
+        {
+
+            int hour = DateTime.Now.Hour;
+
+            string greeting;
+
+            if (hour >= 0 && hour < 12)
+            {
+                Console.WriteLine($"Good Morning''{firstname}''{lastname}");
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                Console.WriteLine($"Good Afternoon''{firstname}''{lastname}");
+
+            }
+            else
+            {
+                Console.WriteLine($"Good Evening''{firstname}''{lastname}");
+
+            }
+
+        }
+    }
+}
--- Greeting Program/Program.cs
+++ Greeting Program/Program.cs
@@ -1,8 +1,8 @@
 ﻿namespace Greeting_Program
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.Write("Please Write Your FirstName : ");
             string firstName = Console.ReadLine();
@@ -10,58 +10,12 @@
             string lastName = Console.ReadLine();
 
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine($"{Abbreviation(firstName, lastName)}");
+            Console.WriteLine($"{GreetingToUser.Abbreviation(firstName, lastName)}");
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Greeting(firstName,lastName);
+            GreetingToUser.Greeting(firstName,lastName);
 
 
         }
 
-        /// <summary>
-        /// به هم چسباندن و بزرگ کردن حرف اول اسم 
-        /// </summary>
-        /// <param name="firstName"></param>
-        /// <param name="lastName"></param>
-        /// <returns></returns>
-
-        public static string Abbreviation(string firstName, string lastName)
-        {
-            string firstNameAbbreviation = firstName.Substring(0, 1).ToUpper();
-            string lastNameAbbreviation = lastName.Substring(0, 1).ToUpper();
-
-            string abbreviation = $"Welcome {firstNameAbbreviation}.{lastNameAbbreviation}";
-
-            return abbreviation;
-        }
-
-
-        /// <summary>
-        /// خوش امد گویی بر طبق زمان روز
-        /// </summary>
-        /// <param name="firstName"></param>
-        /// <param name="lastName"></param>
-        /// <returns></returns>
-        public static string Greeting(string firstname, string lastname)
-        {
-
-            int hour = DateTime.Now.Hour;
-
-            string greeting;
-
-            if (hour >= 0 && hour < 12)
-            {
-                greeting = $"Good Morning''{firstname}''{lastname}";
-            }
-            else if (hour >= 12 && hour < 18)
-            {
-                greeting = $"Good Afternoon''{firstname}''{lastname}";
-            }
-            else
-            {
-                greeting = $"Good Evening''{firstname}''{lastname}";
-            }
-
-            return greeting;
-        }
     }
 }
