5ec63f181af384278d09fd57a3d951f942693613
Hamidreza Pakpour <hamidrezapakpour95@gmail.com>
2023-12-13 09:52:35 +0330

    To Tooman extension method and TO Shamsi extension method Created

---
+++ Extension Methods and Practice Solution Folder/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Extension_Methods_and_Practice_Solution_Folder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(250000000.ToToman());
+
+            Console.WriteLine(DateTime.Now.ToShamsi());
+        }
+    }
+}
--- Tools/ExtensionMethods.cs
+++ Tools/ExtensionMethods.cs
@@ -1,7 +1,29 @@
-﻿namespace Tools
-{
-    public class ExtensionMethods
-    {
+﻿using System.Globalization;
 
+namespace Tools
+{
+    public static class ExtensionMethods
+    {
+        #region ToToman
+        /// <summary>
+        /// برای جداکننده 3 رقمی روی قیمت ها 
+        /// </summary>
+        /// <param name="Amount"></param>
+        /// <returns></returns>
+        public static string ToToman(this int Amount)
+        {
+            return Amount.ToString("#,0 Toman");
+        }
+
+        #endregion
+
+        #region Turn Dates To Persian Date ( ToShamsi )
+        public static string ToShamsi(this DateTime Tarikh)
+        {
+            PersianCalendar pc = new PersianCalendar();
+            return pc.GetYear(Tarikh)+"/"+pc.GetMonth(Tarikh).ToString("00")+
+                   "/"+pc.GetDayOfMonth(Tarikh).ToString("00");
+        }
+        #endregion
     }
 }
