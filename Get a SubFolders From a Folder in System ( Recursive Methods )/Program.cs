e0ec7c4afffa29aeb71dcea04582bb9c158f66bc
Hamidreza Pakpour <hamidrezapakpour95@gmail.com>
2023-12-13 09:56:18 +0330

    Subfolder selection in system project created

---
+++ Get a SubFolders From a Folder in System ( Recursive Methods )/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Get_a_SubFolders_From_a_Folder_in_System___Recursive_Methods__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
